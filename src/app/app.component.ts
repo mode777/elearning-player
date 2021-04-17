eef7e8579e4a6e1e46e8f7655773c2065df546c1
Alexander Klingenbeck <mode7@gmx.net>
2021-04-17 15:36:41 +0200

    Add player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'elearning-player';
+  @ViewChild('video') videoRef;   
+  public get video() : HTMLVideoElement { return this.videoRef.nativeElement }
+  
+  public playText = "Play"
+  public time = 0
+  public duration = 0
+
+  togglePlay(){
+    if (this.video.paused || this.video.ended) {
+      this.video.play();
+      this.playText = "Pause"
+    } else {
+      this.video.pause();
+      this.playText = "Play";
+    }
+  }
+
+  videoLoaded(){
+    this.duration= this.video.duration
+  }
+
+  onUpdate(ev){
+    this.time = this.video.currentTime
+  }
 }
